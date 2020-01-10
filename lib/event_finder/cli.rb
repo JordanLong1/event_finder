35348809543248d2e634926ae67b71ccb628f592
Jordan <jordanleelong5@gmail.com>
2020-01-09 19:05:00 -0800

    took out options method in cli

--- lib/event_finder/cli.rb
+++ lib/event_finder/cli.rb
@@ -8,9 +8,7 @@ class EventFinder::CLI
         sleep 2
         listing
         sleep 2
-        # get_event_method
-        # sleep 2
-        options
+        get_event_method
         sleep 2
         goodbye
     end
@@ -29,39 +27,27 @@ class EventFinder::CLI
     def listing
         puts "Here are the upcoming top events in your area:"
         @events = EventFinder::Scraper.scrape_bands_in_town
-        @events.each.with_index(1) do |event, i|
-            # puts "#{i}. #{event.name}"
+        @events.each.with_index(1) do |event, index|
+            # puts "#{index}. #{events.name}"
         end
 
-        # def get_event_method
-        #     puts "Please enter the number of which event you'd like more info on!"
-        #     input = gets.strip
-        #     index = input.to_i - 1
-        #     if index.to_i.between?(0, 17)
-        #         binding.pry
-        #         @events[index]
-        #         # list event that corresponds with the number input
-        #     else
-        #         puts "I don't recognize that command, please enter a valid number."
-        #         get_event_method
-        #     end
-        # end
-    end
+        def get_event_method
+            puts "Please enter the number of which event you'd like more info on!"
+            input = gets.strip
+            index = input.to_i - 1
+            if index.to_i.between?(0, 17)
+                event_info = @events[index]
+                #puts #{event_info.date} - #{event_info.location}
+            elsif input == "exit"
+                
+                # list event that corresponds with the number input
+                 #events date, location and url for more info
 
-    def options
-        input = nil
-        while input != "exit"
-            input = gets.strip.downcase
-
-            if input.to_i > 0
-                the_event = @events[input.to_i - 1]
-                # puts "#{the_event.name} - #{the_event.location} - #{the_event.url}"
-            elsif input == "list"
-                listing
             else
-                puts "Not sure i recognize your command, type list or exit"
-         end
-      end
+                puts "I don't recognize that command, please enter a valid number."
+                get_event_method
+            end
+        end
     end
 
     def goodbye 
